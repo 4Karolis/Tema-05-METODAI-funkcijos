d8aff395b1d3d0a5cb77c83ef1eabb3e387eef18
4Karolis <4karolis.t@gmail.com>
2022-03-22 22:12:40 +0200

    Created repository

---
+++ Tema 05 - METODAI (funkcijos)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tema_05___METODAI__funkcijos_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
